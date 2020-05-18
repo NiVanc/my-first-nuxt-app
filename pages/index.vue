9c3973b88a31a6f341cd4604240d75e1725f9db7
NiVanc <nico@izebra.be>
2020-05-19 00:10:00 +0200

    Add & style components

--- pages/index.vue
+++ pages/index.vue
@@ -11,16 +11,6 @@
   </div>
 </template>
 
-<script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
-</script>
-
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
